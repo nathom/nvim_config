0ce206e302124f91053b3b9f0779a871f7fff2d6
Nathan Thomas <nathanthomas707@gmail.com>
2025-06-25 11:36:02 -0700

    Fix path

--- lua/plugins/autocorrect.lua
+++ lua/plugins/autocorrect.lua
@@ -1,6 +1,6 @@
 -- Fix commonly misspelled words
 return {
-	dir = "nathom/autocorrect.nvim",
+	"nathom/autocorrect.nvim",
 	ft = { "markdown" },
 	config = function()
 		require("autocorrect").Autocorrect()
